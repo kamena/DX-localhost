10ecf3b388b0c8c22af8e0ba913601f1d4e627ee
mpeshev <mario@peshev.net>
2014-10-12 15:47:07 +0300

    dx-localhost.css

--- dx-localhost.js
+++ dx-localhost.js
@@ -5,7 +5,7 @@
 jQuery( document ).ready( function( $ ) {
 	var notice = dxlocalhost.notice_msg;
 	$('body').append( '<div id="dx-localhost-notice">' + notice + '</div>' );
-	if ( $('#wpadminbar') ) {
+	if ( $('#wpadminbar').length ) {
 		$('#dx-localhost-notice').css('top', '32px');
 	}
 });

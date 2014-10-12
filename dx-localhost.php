ec53b976c727b5de9a029010a0be2ca35461240c
mpeshev <mario@peshev.net>
2014-10-12 15:43:47 +0300

    Update main file with license info

--- dx-localhost.php
+++ dx-localhost.php
@@ -4,8 +4,23 @@
  * Description: Display a yellow notice box when you're working on localhost
  * Author: nofearinc
  * Author URI: http://devwp.eu/
+ * License: GPL2
+
+ Copyright 2014 mpeshev (email : mpeshev AT devrix DOT com)
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License, version 2, as
+ published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
- 
 
 add_action( 'admin_enqueue_scripts', 'dx_localhost_display' ); 
 add_action( 'wp_enqueue_scripts', 'dx_localhost_display' );
